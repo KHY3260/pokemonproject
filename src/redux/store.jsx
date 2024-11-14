4f97b4cf535156b73d6ba29db50027d9a9a0de16
kyh3260 <ppp3260@naver.com>
2024-11-14 09:51:21 +0900

    redux2

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import pokemonReducer from "./pokemonSlice";
+import pokemonReducer from "./redux/pokemonSlice";
 
 const store = configureStore({
   reducer: {
